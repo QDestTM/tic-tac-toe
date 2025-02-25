04ca7f523d4d841e063b56d6ae80635a58eb2ff1
QDestTM <dimag4681@gmail.com>
2025-02-25 11:06:57 +0200

    Added key for internal symbol container in Square component.

--- Shared.tsx
+++ Shared.tsx
@@ -5,12 +5,18 @@ export function sq(index: number): string
 	return `sq-${index}`
 }
 
-// Container key formatter
-export function cn(index: number): string
+// Symbol getter
+export function sym(index: number): string
 {
-	return `cn-${index}`
+	// Even index - X, Odd index - O
+	return index % 2 ? O : X
 }
 
+// Linear interpolation function
+export function lerp(a: number, b: number, x: number): number
+{
+	return a + (b - a) * x
+}
 
 // Symbols constants
 export const X: string = 'x'
@@ -22,7 +28,22 @@ export const D: string = '-'
 export const MinTurn: number = 4
 export const MaxTurn: number = 8
 
+export const StateRange: number[]
+	= Array.from({ length : MaxTurn + 2 }, (_, i) => i)
 
+// Color constants
+export const COLOR_APP: string = 'powderblue'
+
+export const COLOR_PRIMARY_0: string = 'lightseagreen'
+export const COLOR_PRIMARY_1: string = 'teal'
+
+export const COLOR_SECONDARY_0: string = 'linen'
+export const COLOR_SECONDARY_1: string = 'wheat'
+export const COLOR_SECONDARY_2: string = 'burlywood'
+export const COLOR_SECONDARY_3: string = 'bisque'
+
+
+// Pattern constant
 export const WinningPatterns = [
 	[sq(0), sq(1), sq(2)], // Horizontal lines
 	[sq(3), sq(4), sq(5)],
